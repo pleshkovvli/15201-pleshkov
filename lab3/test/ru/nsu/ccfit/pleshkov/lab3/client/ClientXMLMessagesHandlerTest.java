e7b875d44947744cbd36c5f21f4488e10b4211c3
Vladimir Pleshkov <pleshkovvli@gmail.com>
2017-06-07 14:42:22 +0700

    Small changes

--- lab3/test/ru/nsu/ccfit/pleshkov/lab3/client/ClientXMLMessagesHandlerTest.java
+++ lab3/test/ru/nsu/ccfit/pleshkov/lab3/client/ClientXMLMessagesHandlerTest.java
@@ -1,7 +1,12 @@
 package ru.nsu.ccfit.pleshkov.lab3.client;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ClientXMLMessagesHandlerTest {
+    @Test
+    public void test() {
 
+    }
 }
